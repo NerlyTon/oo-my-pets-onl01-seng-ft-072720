6d47d3795eeac96ee8b8f637d0dc7315611f7a33
Nerly Ton <nerlyton@gmail.com>
2020-08-12 00:45:38 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,35 @@
 class Owner
-  # code goes here
+  
+  attr_reader :name, :species
+  attr_accessor :cats
+  
+  @@all = []
+  
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    @cats = []
+  end
+  
+  def say_species
+    return "I am a #{@species}."
+  end
+  
+  def self.all 
+   @@all
+  end
+  
+  def self.count
+    self.all.length
+  end
+  
+  def self.reset_all
+    self.all.clear
+  end
+  
+  def cats 
+    return @cats.all
+  end
 end
